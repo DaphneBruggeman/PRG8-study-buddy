14f479da94ce083e626e276268462116de2337c0
Daphne <0986809@hr.nl>
2024-06-16 18:55:48 +0200

    Comments for the video

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+// conectie 
 const { ChatOpenAI } = require("@langchain/openai");
 
 const app = express();
@@ -15,11 +16,12 @@ function createPrompt(history) {
     return prompt;
 }
 
-// Endpoint for chat query
+// einde voor chat query
 app.post("/chat", async (req, res) => {
   const { query, history } = req.body;
   console.log("Received query:", query);
 
+// Key's bekijken 
   try {
     const model = new ChatOpenAI({
       azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
@@ -28,9 +30,14 @@ app.post("/chat", async (req, res) => {
       azureOpenAIApiDeploymentName: process.env.ENGINE_NAME,
     });
 
+//Dit wordt gebruikt om een prompt te maken voor openAI op basis van de voorgestelde vragen van de gebruiker
     const prompt = createPrompt(history);
+
+// Aanroepen van de api van open AI
     const response = await model.invoke(prompt);
     console.log("Response from OpenAI:", response.content);
+
+// Antwoord van openAI wordt terug gestuurd naar de client in json. 
     res.json({ response: response.content });
   } catch (error) {
     console.error("Error invoking ChatGPT:", error);
@@ -38,7 +45,7 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-// Start the server
+// Start de server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running`);
 });
